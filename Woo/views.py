8e9844237195c21d76a80439273ec3e768faec7a
Andriuman <160645859+We-Solve-It-Out@users.noreply.github.com>
2024-04-01 22:47:51 -0500

    01_04_2024

--- Woo/views.py
+++ Woo/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .Woo_Script import ejecutar_script
 
+@csrf_exempt  # Deshabilita la verificación de CSRF para esta vista específica
 def IntegrarScript(request):
-    ejecutar_script()
-    return HttpResponse("Script ejecutado.")
+    if request.method == "POST":
+        ejecutar_script()
+        return HttpResponse("Script ejecutado para POST.")
+    else:
+        # Podrías manejar otros métodos HTTP aquí, como GET, si fuera necesario
+        return HttpResponse("Este método HTTP no está permitido.", status=405)
